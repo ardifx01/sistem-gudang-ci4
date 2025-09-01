aecdbb5c6d70b656008e855bdfb16a440e8ba6db
Zulkarnain <zulkarnain.tech@gmail.com>
2025-09-01 23:21:05 +0700

    Update Front End - Js

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -10,22 +10,22 @@
     </aside>
   </div>
 <script src="<?= base_url('adminlte/plugins/jquery/jquery.min.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/jquery-ui/jquery-ui.min.js'); ?>"></script>
+<!-- <script src="<?= base_url('adminlte/plugins/jquery-ui/jquery-ui.min.js'); ?>"></script> -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
-<script src="<?= base_url('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/chart.js/Chart.min.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/sparklines/sparkline.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/jqvmap/jquery.vmap.min.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/moment/moment.min.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/daterangepicker/daterangepicker.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/summernote/summernote-bs4.min.js'); ?>"></script>
-<script src="<?= base_url('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'); ?>"></script>
-<script src="<?= base_url('adminlte/dist/js/adminlte.js'); ?>"></script>
-<script src="<?= base_url('adminlte/dist/js/demo.js'); ?>"></script>
+<!-- <script src="<?= base_url('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/chart.js/Chart.min.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/sparklines/sparkline.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/jqvmap/jquery.vmap.min.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/moment/moment.min.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/daterangepicker/daterangepicker.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/summernote/summernote-bs4.min.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/dist/js/adminlte.js'); ?>"></script> -->
+<!-- <script src="<?= base_url('adminlte/dist/js/demo.js'); ?>"></script> -->
 
 <!-- DataTables  & Plugins -->
 <script src="<?= base_url('adminlte/plugins/datatables/jquery.dataTables.min.js'); ?>"></script>
